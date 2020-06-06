9c01d98cdb1ce7679a020df927661aac9a937843
ench0 <ensar@farend.net>
2020-06-06 16:06:47 +0100

    use duration for hour fraction conversion

--- lib/src/func/helpers.dart
+++ lib/src/func/helpers.dart
@@ -25,18 +25,31 @@ DateTime hourFractionToDateTime(
   // adjust times for dst
   int dstAdjust = summerTimeCalc && isDSTCalc(date) ? 1 : 0;
 
-  int hour;
-  if (hourFraction != double.nan && hourFraction != null)
-    hour = hourFraction.floor();
-  else
-    hour = 23;
+  Duration totalSeconds = Duration(seconds: (hourFraction * 3600).round());
 
-  int minute = showSeconds
-      ? ((hourFraction - hour) * 60).floor()
-      : ((hourFraction - hour) * 60).round(); // rounding minutes
+  int hour = totalSeconds.inHours;
+  int minute = totalSeconds.inMinutes.remainder(60);
+  int second = totalSeconds.inSeconds.remainder(60);
 
-  int second = showSeconds ? (hourFraction - hour - minute).floor : 0;
+  // rounding
+  if (second > 30 && !showSeconds) minute++;
+  if (minute == 60) minute = 0;
 
+  // int hour;
+  // if (hourFraction != double.nan && hourFraction != null)
+  //   hour = hourFraction.floor();
+  // else
+  //   hour = 23;
+
+  // int minute = showSeconds
+  //     ? ((hourFraction - hour) * 60).floor()
+  //     : ((hourFraction - hour) * 60).round(); // rounding minutes
+
+  print('###\nhourFraction: $hourFraction\thour: $hour\tminute: $minute');
+
+  // int second = showSeconds ? (hourFraction - hour - minute / 60).floor : 0;
+
+  print('***** second: $second');
   return DateTime(date.year, date.month, date.day, hour, minute, second)
       .add(Duration(hours: dstAdjust));
 }
