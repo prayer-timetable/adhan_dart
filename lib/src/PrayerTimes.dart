861b638ce86075778608c3158e1a777cd5fa89c6
Mahmoud Hamdi <hmdy7486@gmail.com>
2026-03-13 17:31:21 +0200

    feat: make currentPrayer date optional and add toString
    Make the date parameter in currentPrayer() optional with a default of
    DateTime.now(), consistent with nextPrayer(). Also add toString()
    override for better debugging output.

--- lib/src/PrayerTimes.dart
+++ lib/src/PrayerTimes.dart
@@ -227,11 +227,9 @@ class PrayerTimes {
 
   /// Returns the current prayer for the given date.
   ///
-  /// [date] - Date/time to check against prayer times
-  Prayer currentPrayer({required DateTime date}) {
-    // if (date == null) {
-    //   date = DateTime.now();
-    // }
+  /// [date] - Optional date/time to check against. Defaults to DateTime.now()
+  Prayer currentPrayer({DateTime? date}) {
+    date ??= DateTime.now();
     if (date.isAfter(isha)) {
       return Prayer.isha;
     } else if (date.isAfter(maghrib)) {
@@ -291,5 +289,11 @@ class PrayerTimes {
       case Prayer.fajrAfter:
         return fajrAfter;
     }
+  }
+
+  @override
+  String toString() {
+    return 'PrayerTimes(fajr: $fajr, sunrise: $sunrise, dhuhr: $dhuhr, '
+        'asr: $asr, maghrib: $maghrib, isha: $isha)';
   }
 }
